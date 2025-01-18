1b40c261949b20179ae6bdd9b0adfcb7a2b7c469
WLi0777 <129905625+WLi0777@users.noreply.github.com>
2025-01-18 14:59:34 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="LearnTracking",
+    version="0.1",
+    packages=["dataprep"],  
+    install_requires=[
+        "opencv-python",
+        "pyyaml",
+    ],
+    description="A library for learning tracking",
+    author="WL",
+    author_email="wl0777@outlook.com",
+    url="https://github.com/WLi0777/LearnTracking",
+)
