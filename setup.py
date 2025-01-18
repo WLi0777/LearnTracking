ed66fd92851daeb4191e18aa888522a031b1cdb0
WLi0777 <129905625+WLi0777@users.noreply.github.com>
2025-01-18 20:44:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         "opencv-python",
         "pyyaml",
-        "deeplabcut",
+        "deeplabcut==2.3.9",
         "tensorpack", 
     ],
     description="A library for learning tracking",
