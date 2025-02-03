0a19edf2c07f0cf5bdba59e056104766e6bc8868
WLi0777 <129905625+WLi0777@users.noreply.github.com>
2025-02-03 15:19:26 +0000

    Update setup_folders.py

--- dataprep/setup_folders.py
+++ dataprep/setup_folders.py
@@ -16,3 +16,22 @@ def create_folders(video_folder="ori_video", frames_folder="frames_labeling", la
     os.makedirs(total_frames, exist_ok=True)
 
     print(f"Created folders: {video_folder}, {frames_folder}, {labeled_folder}, {total_frames}")
+
+
+def create_folders_yolo(video_folder="ori_video", frames_folder="frames_labeling", labeled_folder="yolo_folder", total_frames="frames_labeling/total_video_frames"):
+    """
+    Create necessary folders to avoid duplicates.
+    
+    Args:
+        video_folder (str): Path to the video folder
+        frames_folder (str): Path to the frames folder
+        labeled_folder (str): Path to the labeled folder
+        total_frames (str): Path to the total_video_frames folder
+    """
+    os.makedirs(video_folder, exist_ok=True)
+    os.makedirs(frames_folder, exist_ok=True)
+    os.makedirs(labeled_folder, exist_ok=True)
+    os.makedirs(total_frames, exist_ok=True)
+
+    print(f"Created folders: {video_folder}, {frames_folder}, {labeled_folder}, {total_frames}")
+
